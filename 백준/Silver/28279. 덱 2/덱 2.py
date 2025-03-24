224c8815d706e0235db1aab3351424713c9d52d9
Seoyoung Lee <79400606+seoyoung-e@users.noreply.github.com>
2025-03-24 17:41:45 +0900

    [Silver IV] Title: 덱 2, Time: 1356 ms, Memory: 72568 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -1,29 +1,15 @@
 import sys
 from collections import deque
-
-n = int(sys.stdin.readline())  # 일관성 유지: input() → sys.stdin.readline()
 q = deque()
 
-for _ in range(n):
+for _ in range(int(sys.stdin.readline()) ):
     x = list(map(int, sys.stdin.readline().split()))
-
-    if len(x) > 1:
-        num = x[1]
-        if x[0] == 2:  # 🔹 올바르게 정수 비교 수행
-            q.appendleft(num)
-        else:
-            q.append(num)
-    else:
-        num = x[0]
-        if num == 3:
-            print(q.pop() if q else -1)
-        elif num == 4:
-            print(q.popleft() if q else -1)
-        elif num == 5:
-            print(len(q))
-        elif num == 6:
-            print(1 if not q else 0)
-        elif num == 7:
-            print(q[-1] if q else -1)
-        else:
-            print(q[0] if q else -1)
+    match x[0]:
+        case 1: q.append(x[1])
+        case 2: q.appendleft(x[1])
+        case 3: print(q.pop() if q else -1)    
+        case 4: print(q.popleft() if q else -1)
+        case 5: print(len(q))
+        case 6: print(1 if not q else 0)
+        case 7: print(q[-1] if q else -1)
+        case 8: print(q[0] if q else -1)
